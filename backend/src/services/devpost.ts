dc5599904cc4b4179e9b87b20510266a719092ed
Muhammad Khatri <muhammad@weareasterisk.com>
2020-02-04 02:29:22 -0600

    chore: wip - init gavel data conformation

--- backend/src/services/devpost.ts
+++ backend/src/services/devpost.ts
@@ -1,7 +1,22 @@
 import { Request, Response } from "express"
+import multer from "multer"
 
+const upload = multer({})
+
+/**
+ *
+ * @param {Request} req
+ * @param {Response} res
+ */
 export const convertDevpostToGavelCSV = async (req: Request, res: Response) => {
   res.json({
     req,
   })
 }
+
+export const convertDevpostUploadToGavelConsumableData = [
+  upload.single("file"),
+  async (req: Request, res: Response) => {
+    const { file } = req
+  },
+]
